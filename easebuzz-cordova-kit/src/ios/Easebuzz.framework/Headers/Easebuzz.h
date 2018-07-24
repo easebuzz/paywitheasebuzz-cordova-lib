6b3179bf485e2cd6c573e6109923cf381fa28117
Pradip Dighe <pradip@srvmedia.com>
2018-07-24 20:34:01 +0530

    added the kit

---
+++ easebuzz-cordova-kit/src/ios/Easebuzz.framework/Headers/Easebuzz.h
@@ -0,0 +1,19 @@
+//
+//  Easebuzz.h
+//  Easebuzz
+//
+//  Created by Pro Retina on 14/12/17.
+//  Copyright © 2017 Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Easebuzz.
+FOUNDATION_EXPORT double EasebuzzVersionNumber;
+
+//! Project version string for Easebuzz.
+FOUNDATION_EXPORT const unsigned char EasebuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Easebuzz/PublicHeader.h>
+
+
